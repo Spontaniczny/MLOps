302485bfc30ec816adf05cf2e39701469f9ac0e2
Spontaniczny <k2002kuba@gmail.com>
2025-12-22 17:32:33 +0100

    Lab 10 excersice 1 done!

---
+++ lab10/excercise1/main.tf
@@ -0,0 +1,14 @@
+# main.tf
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
---
+++ lab10/excercise1/outputs.tf
@@ -0,0 +1,7 @@
+# outputs.tf
+
+
+output "repository_url" {
+  value       = github_repository.example.html_url
+  description = "URL of the created repository"
+}
